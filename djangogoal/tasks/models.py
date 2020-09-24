bf75da8b6a5bd9ff2f36fcfe8e3ed7d6f113e100
Ray <balafranzraymond@gmail.com>
2020-09-24 23:56:14 +0800

    Updated task model

--- djangogoal/tasks/models.py
+++ djangogoal/tasks/models.py
@@ -3,9 +3,23 @@ from django.db import models
 
 # Create your models here.
 
+
 class Task(models.Model):
+    HIGHPRIORITY = "HP"
+    NORMALPRIORITY = "NP"
+    LOWPRIORITY = "LP"
+    EMERGENCY = "SOS"
+    RESCHEDULE = "RS"
+    PRIORITY_RANKS = [
+    (HIGHPRIORITY, 'High Priority'),
+    (NORMALPRIORITY, 'Normal Priority'),
+    (LOWPRIORITY, 'Low Priority'),
+    (EMERGENCY, 'Emergency'),
+    (RESCHEDULE, 'Reschedule'),
+]
+
     activity = models.CharField(max_length=255)
-    priority = models.PositiveIntegerField()
+    priority = models.CharField(choices=PRIORITY_RANKS, default=NORMALPRIORITY, max_length=3)
 
     def __str__(self):
         return self.activity
