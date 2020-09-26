5bd30fc1a34880b9286b8d1ea97ea0349358f571
Ray <balafranzraymond@gmail.com>
2020-09-26 10:23:18 +0800

    Updated views and db

--- djangogoal/tasks/views.py
+++ djangogoal/tasks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.view import APIView
+from rest_framework.response import Response
+
+from . import models
+from . import serializers
+
