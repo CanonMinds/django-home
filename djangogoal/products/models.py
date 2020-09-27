0edf10a68438def6963eceb8e02f2cd2f17acfb5
Ray <balafranzraymond@gmail.com>
2020-09-27 15:42:08 +0800

    Added @property for total of quantities and prices

--- djangogoal/products/models.py
+++ djangogoal/products/models.py
@@ -38,13 +38,13 @@ class Order(models.Model):
 
     @property
     def get_cart_total(self):
-        orderitems = self.orderitem_set.all()
+        orderitems = self.orderitem_set.all() #More on this!
         total = sum([item.get_total for item in orderitems])
         return total
 
     @property
     def get_cart_items(self):
-        orderitems = self.orderitem_set.all()
+        orderitems = self.orderitem_set.all() #More on this!
         total = sum([item.quantity for item in orderitems])
         return total
     
