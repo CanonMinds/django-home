64e87a9f8718a3ec9f173b996dcb06e937d7007c
Ray <balafranzraymond@gmail.com>
2020-09-28 15:54:51 +0800

    Added the shipping validation and checkout

--- djangogoal/products/views.py
+++ djangogoal/products/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.http import JsonResponse
+import datetime
 import json
 
 # Create your views here.
@@ -91,6 +92,32 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+    
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                barangay=data['shipping']['barangay'],
+                zipcode=data['shipping']['zipcode'],
+
+            )
+    else:
+        print ('User is not logged in.')
+
     print('Data: ',request.body )
     return JsonResponse('Payment complete!', safe=False)
 
