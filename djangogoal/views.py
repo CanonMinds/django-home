cf45b55e4685211be4fb70ed19e7b1f9b348159b
Ray <balafranzraymond@gmail.com>
2020-10-05 00:33:22 +0800

    Updated auth

--- djangogoal/views.py
+++ djangogoal/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
+
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
-# def home(request):
-#     return render(request,'home.html')
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -26,11 +27,25 @@ def signupPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
 
     context = {'form':form}
     return render(request, 'signup.html', context)
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     context = {}
     return render(request, 'login.html', context)
 
