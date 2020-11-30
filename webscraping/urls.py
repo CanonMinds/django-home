8dbe2cda877387f36c4734c6d8fa98d61ca2f70f
Ray <balafranzraymond@gmail.com>
2020-11-30 18:01:16 +0800

    Added new views/templates for init webscrape

--- webscraping/urls.py
+++ webscraping/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'webscraping'
+app_name = 'webscraper'
 
 urlpatterns = [
     # url(r'^$', views.ProductsView.as_view(), name='products_list'),
-    url(r'^$', views.WebListView.as_view(), name="webscraping_list"),
+    url(r'^$', views.home, name="webscraping_list"),
+    url(r'^module1/$', views.module1, name="module1_list"),
+    url(r'^module2/$', views.module2, name="module2_list"),
+    url(r'^module3/$', views.module3, name="module3_list"),
 ]
--- webscraping/views.py
+++ webscraping/views.py
@@ -14,50 +14,124 @@ import time
 from time import sleep
 
 from . import models
+ 
+def get_html_content(city):
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)    
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    city = city.replace('','+')
+    html_content = session.get(f'https://www.google.com/search?q=weather+in+{city}')
+    return html_content
 
-class WebListView(ListView):
-    context_object_name = "webdata"
-    # fields = ("municipality", "region")
-    # model = LocalShelterPlan, Region
-    template_name = "webscraping/webscraping_list.html"
+def home(request):
+    #First Scraping
+    # source = requests.get('http://coreyms.com').text
+    # soup = BeautifulSoup(source, 'lxml')
+    # web_data = None
+    # for article in soup.find_all('article'):
+    #     web_data = {}
+    #     web_data['headline'] = article.h2.a.text
+    #     web_data['summary'] = article.find('div', class_='entry-content').p.text
+    #     try:
+    #         vid_src = article.find('iframe', class_='youtube-player')['src']
+    #         vid_id = vid_src.split('/')[4]
+    #         vid_id = vid_id.split('?')[0]
+    #         web_data['yt_link'] = f'https://youtube.com/watch?v={vid_id}'
+    #     except Exception as e:
+    #         web_data['yt_link'] = None
+    
+    #Second Scraping with Issues
+    # Has <Response 429> Issues
+    # weather_data = None
+    # if 'city' in request.GET:
+        #Fetching the data
+        # city = request.GET.get('city')
+        # html_content = get_html_content(city)
+        # soup = BeautifulSoup(html_content, 'html.parser')
 
-    # url = form.cleaned_data.get['http://www.unece.org/cefact/locode/service/location']
-    # data = requests.get(url)
-    def __init__(self):
-        print("Starting Now")
-        self.get_html_content    
-
-    def get_html_content(self, city):
-        USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
-        LANGUAGE = "en-US,en;q=0.5"
-        session = requests.Session()
-        retry = Retry(connect=3, backoff_factor=0.5)
-        adapter = HTTPAdapter(max_retries=retry)    
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        session.headers['User-Agent'] = USER_AGENT
-        session.headers['Accept-Language'] = LANGUAGE
-        session.headers['Content-Language'] = LANGUAGE
-        city = city.replace('','+')
-        html_content = session.get(f'https://www.google.com/search?q=weather+in+{city}').text
-        # response = HttpResponse(html_content)
-        # print(response)
-        # if response.status_code == 429:
-        #     time.sleep(int(response.headers["Retry-After"]))
-        # URL = 'https://www.google.com/search?q=weather+in+{city}'
-        # html_content = requests.get(URL)
+        # weather_data = dict()
+        # weather_data['region'] = soup.find('div', attrs={'id': 'wob_loc'}).text
+        # weather_data['daytime'] = soup.find('div', attrs={'id': 'wob_dts'}).text
+        # weather_data['status'] = soup.find('div', attrs={'id': 'wob_dc'}).text
+        # weather_data['temp'] = soup.find('div', attrs={'id': 'wob_tm'}).text
         # print(html_content)
-        return html_content
+        
+        # print(weather_data)
+        # pass
 
-    def get(self, request):
-        if 'city' in request.GET:
-            #Fetching the data
-            city = request.GET.get('city')
-            html_content = self.get_html_content(city)
-            print(html_content)
-            pass
-        # lsp_plans_list = LocalShelterPlan.objects.all()
-        # region = Region.objects.order_by('name')
-        # lsp_plans_count = LocalShelterPlan.objects.all().count()
-        # context_data = {'lsp_plans_count': lsp_plans_count, 'lsp_plans_list': lsp_plans_list, 'region': region}
-        return render(request, self.template_name)
+    return render(request, 'webscraping/webscraping_list.html',)
+
+def module1(request):
+    #Third Scraping
+    url1 = "https://timesofindia.indiatimes.com/briefs"
+    toi_r = requests.get(url1)
+    toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+    toi_headings = toi_soup.find_all('h2')
+    toi_headings = toi_headings[0:-13]
+    
+    toi_news = []
+
+    for th in toi_headings:
+        toi_news.append(th.text)
+
+    ht_r = requests.get("https://www.hindustantimes.com/india-news/")
+    ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
+    ht_headings = ht_soup.findAll("div", {"class": "headingfour"})
+    ht_headings = ht_headings[2:]
+    ht_news = []
+
+    for hth in ht_headings:
+        ht_news.append(hth.text)
+
+    return render(request, 'webscraping/webscraping_module1.html', 
+    {'toi_news': toi_news, 'ht_news': ht_news , 'url1': url1, })
+
+def module2(request):
+    #Fourth Scraping
+    url1 = "https://us.cnn.com/us"
+    cnn_r = requests.get(url1)
+    cnn_soup = BeautifulSoup(cnn_r.content, 'html5lib')
+    cnn_headings = cnn_soup.find_all("span", class_="cd__headline-text")
+    cnn_headings = cnn_headings[0:] # removing footers
+    cnn_headlines = []
+
+    for th in cnn_headings:
+        cnn_headlines.append(th.text)
+
+    return render(request, 'webscraping/webscraping_module2.html', 
+    {'cnn_headlines': cnn_headlines, 'url1': url1, })
+
+def module3(request):
+    #Fourth Scraping
+    url1 = "https://www.philstar.com/headlines"
+    pr_r = requests.get(url1)
+    pr_soup = BeautifulSoup(pr_r.content, 'html5lib')
+    pr_headings = pr_soup.find_all("div", class_="news_title")
+    pr_headingsTop = pr_soup.find_all("div", class_="topNews_title")
+    pr_headingsSlider = pr_soup.find_all("div", class_="slider_title")
+
+    pr_headings = pr_headings[0:]
+    pr_headingsTop = pr_headingsTop[0:]
+    pr_headingsSlider = pr_headingsSlider[0:]
+
+    pr_headlines = []
+
+    for th in pr_headingsSlider:
+        pr_headlines.append(th.text)
+
+    for th in pr_headingsTop:
+        pr_headlines.append(th.text)
+
+    for th in pr_headings:
+        pr_headlines.append(th.text)
+
+    print(pr_headlines)
+    return render(request, 'webscraping/webscraping_module3.html', 
+    {'pr_headlines': pr_headlines, 'url1': url1, })
