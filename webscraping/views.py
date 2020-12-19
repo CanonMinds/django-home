95339e49d91292b7c62e37bbf91f5d0219dec154
Ray <balafranzraymond@gmail.com>
2020-12-19 22:16:38 +0800

    Removed unused imports

--- webscraping/views.py
+++ webscraping/views.py
@@ -11,7 +11,6 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from django.http import HttpResponse    
 import time
-from time import sleep
 
 from . import models
  
