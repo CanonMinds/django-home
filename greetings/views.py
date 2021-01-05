e012e25c13001df609c1d01299e363b4b9cd2157
Ray <balafranzraymond@gmail.com>
2021-01-05 23:23:25 +0800

    Refactor greetings

--- greetings/views.py
+++ greetings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def flashword(request, username):
     template_name = 'greetings/index.html'
     context = {'username' : username,}
