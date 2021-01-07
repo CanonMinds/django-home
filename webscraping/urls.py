4368bf25a1ce7ec12ccd4c3dfb4e5f53330885b0
Ray <balafranzraymond@gmail.com>
2021-01-08 00:00:21 +0800

    Removed unused Webscraping url

--- webscraping/urls.py
+++ webscraping/urls.py
@@ -7,7 +7,6 @@ from . import views
 app_name = 'webscraper'
 
 urlpatterns = [
-    # url(r'^$', views.ProductsView.as_view(), name='products_list'),
     url(r'^$', views.home, name="webscraping_list"),
     url(r'^module1/$', views.module1, name="module1_list"),
     url(r'^module2/$', views.module2, name="module2_list"),
