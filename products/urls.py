2a2c1d447185de374dafc4b8873c024295b8d200
Ray <balafranzraymond@gmail.com>
2021-01-08 20:19:19 +0800

    Update urls in products app

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    # url(r'^$', views.ProductsView.as_view(), name='products_list'),
     url(r'^$', views.store, name="products_list"),
     url(r'^cart/$', views.cart, name="cart"),
     url(r'^checkout/$', views.checkout, name="checkout"),
